4d1ef20069822f798548a79c00093b8e2f66a1c6
FranPerezRojas <66255260+FranPerezRojas@users.noreply.github.com>
2020-06-02 16:31:29 +0200

    cambios

--- aplicacion1/src/bienvenido.java
+++ aplicacion1/src/bienvenido.java
@@ -13,6 +13,10 @@ public class bienvenido {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double delta = 0.0000000001;
+	double x0=1,x1=3,y0=2,y1=4; 
+	
+	
 
 	}
 
