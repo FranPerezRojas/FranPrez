262dc71f08bb3a7ff9f852bf2dd84d8ce4559ad6
FranPerezRojas <66255260+FranPerezRojas@users.noreply.github.com>
2020-06-02 16:35:31 +0200

    Update bienvenido.java

--- aplicacion1/src/bienvenido.java
+++ aplicacion1/src/bienvenido.java
@@ -13,8 +13,8 @@ public class bienvenido {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double delta = 0.0000000001;
-	double x0=1,x1=3,y0=2,y1=4; 
+		double delta = 0.01;
+	double x0=1,x1=3,y0=2,y1=5; 
 	
 	
 
